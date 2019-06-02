22e089a80131fb513c6bd664e7664871bfd6a04c
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-06-02 09:26:32 -0700

    static.json added to fix routing error

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/cart" component={Cartz} />
-          <Route exact path="*" component={Home} />
-        </Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/cart" component={Cartz} />
+        <Route exact path="*" component={Home} />
       </Router>
     );
   }
