874b8b595c76e60dc0b84e59bc2f580a02bfdf70
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-06-02 08:24:07 -0700

    service worker modified

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
 import { loadState, saveState } from "./localStorage";
 import { createStore } from "redux";
