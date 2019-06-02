98fc52a37dc1c346693fff58172089e4d2e4fb83
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-06-02 09:46:52 -0700

    static.json removed

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./componenets/Home/Home";
 import Cartz from "./componenets/Cartz/Cartz";
+import Test from "./componenets/Test";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/cart/" component={Cartz} />
+          <Route exact path="/Test" component={Test} />
           <Route exact path="*" component={Home} />
         </Switch>
       </Router>
---
+++ client/src/componenets/Test/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Test extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        lorem ipsum lorem ipsum joe doe ti dockrathi venned yi gucko taye taye.
+      </div>
+    );
+  }
+}
+
+export default Test;
